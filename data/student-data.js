67e69cdb7d3492f075ee0976b048e259d43ab65f
Daniel Mathews <daniel.g.mathews@gmail.com>
2023-04-10 21:52:58 -0400

    draft version 0

---
+++ data/student-data.js
@@ -0,0 +1,38 @@
+const students = [
+  {
+    name: 'Alice',
+    age: 20,
+    grade: 85,
+    major: 'Computer Science'
+  },
+  {
+    name: 'Bob',
+    age: 21,
+    grade: 78,
+    major: 'Mathematics'
+  },
+  {
+    name: 'Charlie',
+    age: 19,
+    grade: 92,
+    major: 'English Literature'
+  },
+  {
+    name: 'David',
+    age: 22,
+    grade: 70,
+    major: 'History'
+  },
+  {
+    name: 'Eve',
+    age: 20,
+    grade: 90,
+    major: 'Biology'
+  }
+]
+
+export {
+  students
+}
+
+
